440b29c8338e1424c22d0cc8bf19a01030850140
Sadek-Hossain-Nishat <sadekhossainnishat@gmail.com>
2022-12-11 18:48:42 -0800

    fourth commit

---
+++ app/src/main/java/com/example/mvvmimagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.example.mvvmimagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.example.mvvmimagesearchapp.R
+
+class GalleryFragment:Fragment(R.layout.fragment_gallery) {
+}
